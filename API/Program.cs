1ce9e6c70f0005a8aafe78ced954667fc5c47607
Huong Nguyen <huongnguyen10102@gmail.com>
2022-11-22 19:15:45 +0200

    Creating the db

--- API/Program.cs
+++ API/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;
 
 namespace API
 {
@@ -13,7 +15,27 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            // Scope will host any services that created inside the method
+            using var scope = host.Services.CreateAsyncScope();
+
+            var services = scope.ServiceProvider;
+
+            try
+            {
+                // if do not have any db when starting app, this line of code is run
+                // Will run the migration and create db
+                var context = services.GetRequiredService<DataContext>();
+                context.Database.Migrate();
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occured during migration");
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
