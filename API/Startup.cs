a471260274afcdc1805f4a0e620f30aa67e742dd
Huong Nguyen <huongnguyen10102@gmail.com>
2022-12-01 23:08:02 +0200

    Adding CORS Policy, so the browser can receive the data from API

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,6 +36,15 @@ namespace API
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+
+            // add CORS Policy so UI can fetch the data
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +60,8 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
